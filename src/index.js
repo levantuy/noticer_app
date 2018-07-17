5c3c38ab97f7b9567729f00b68139976a24600e2
levantuy <levantuy.it@gmail.com>
2018-07-17 20:07:37 +0700

    add firebase hosting

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-localStorage.setItem("urlApi", 'http://api.t1store.com/api/');
+localStorage.setItem("urlApi", 'http://flashlightvn.com/api/');
 // localStorage.setItem("urlApi", 'http://localhost:53875/api/');
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
